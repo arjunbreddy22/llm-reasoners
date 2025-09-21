c386c6c68498e5384b6716be77484f8ff979bc50
arjunbreddy22 <arjunreddy5275@gmail.com>
2025-09-21 18:30:03 -0500

    Increase token limits from 512 to 1024 because reasoning might be being cut off.

--- examples/ToT/game24_mcts/inference.py
+++ examples/ToT/game24_mcts/inference.py
@@ -158,7 +158,7 @@ if __name__ == '__main__':
             import os
             from reasoners.lm import SGLangModel
             os.environ["SGLANG_API_URL"] = sglang_url
-            base_model = SGLangModel(sglang_model, max_new_tokens=512, is_instruct_model=True)
+            base_model = SGLangModel(sglang_model, max_new_tokens=1024, is_instruct_model=True)
         else:
             assert False, f'cannot resolve {base_lm=}'
         mcts_tot_game24(base_model=base_model,
