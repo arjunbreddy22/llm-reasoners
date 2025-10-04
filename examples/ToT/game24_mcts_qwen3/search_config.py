117f2dfc849b7714b7485e5b503d8cdc1215a385
arjunbreddy22 <arjunreddy5275@gmail.com>
2025-10-04 16:53:09 -0500

    Set action generation tokens max to 1024 from 512

--- examples/ToT/game24_mcts_qwen3/search_config.py
+++ examples/ToT/game24_mcts_qwen3/search_config.py
@@ -260,7 +260,7 @@ class Game24Config(SearchConfig):
                 [prompt],
                 num_return_sequences=1,
                 do_sample=False,
-                max_new_tokens=512,
+                max_new_tokens=1024,
                 eos_token_id='Input',
             ).text[0]
             print(f'DEBUG: Raw model output: {repr(output)}')
